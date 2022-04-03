9739894deb463a29b1232c622f59b65ba176fea5
Shahzad Hafeez <shahzadhafeez63@gmail.com>
2022-04-03 05:53:48 +0200

    added story banner on homepage

--- besv-storefront/cartridges/app_besv_storefront/cartridge/experience/components/assets/storyBanner.js
+++ besv-storefront/cartridges/app_besv_storefront/cartridge/experience/components/assets/storyBanner.js
@@ -7,22 +7,26 @@ var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransfo
 var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
 
 /**
- * Render logic for the assets.awardBanner.
+ * Render logic for the assets.storyBanner.
  */
 module.exports.render = function (context) {
     var model = new HashMap();
     var content = context.content;
 
+    // look
+    model.bannerMargin = content.banner_margin == '0' ? 'no-margin-class' : 'margin-class';
+    model.gradientBackground = content.gradient_background == true ? 'gradient-background-class' : '';
+
     // image
     model.bannerImage = ImageTransformation.getScaledImage(content.banner_image);
+    model.imagePosition = content.image_position || 'left';
     // text
     model.heading = content.text_heading;
     model.description = content.text_description;
+    model.textPosition = model.imagePosition == 'left' ? 'right' : 'left';
     // button
     model.buttonLink = content.button_link;
     model.buttonName = content.button_name || "SHOP NOW";
-    // awards
-    model.regions = PageRenderHelper.getRegionModelRegistry(context.component);
 
-    return new Template('experience/components/assets/awardBanner').render(model).text;
+    return new Template('experience/components/assets/storyBanner').render(model).text;
 };
