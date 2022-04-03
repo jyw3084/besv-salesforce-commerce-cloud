31abd059c1bdff386d92588e8fd31b3469b6f195
Shahzad Hafeez <shahzadhafeez63@gmail.com>
2022-04-03 06:06:58 +0200

    added heroBanner on homepage.

---
+++ besv-storefront/cartridges/app_besv_storefront/cartridge/experience/components/assets/heroBanner.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
+
+/**
+ * Render logic for the assets.heroBanner.
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var content = context.content;
+    // image
+    model.bannerImage = ImageTransformation.getScaledImage(content.banner_image);
+    // text
+    model.heading = content.text_heading;
+    model.description = content.text_description;
+    // button
+    model.buttonLink = content.button_link;
+    model.buttonName = content.button_name || "SHOP NOW";
+
+    return new Template('experience/components/assets/heroBanner').render(model).text;
+};
