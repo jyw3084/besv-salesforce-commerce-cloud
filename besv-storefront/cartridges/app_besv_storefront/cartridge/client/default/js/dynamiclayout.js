79198888757ad585c90a3d2475f8e4024e254fc2
Zeeshan Munawar <zeeshanmunawar981@gmail.com>
2022-04-12 15:36:47 +0500

    Sign up and hero banner

--- besv-storefront/cartridges/app_besv_storefront/cartridge/client/default/js/dynamiclayout.js
+++ besv-storefront/cartridges/app_besv_storefront/cartridge/client/default/js/dynamiclayout.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function () {
-    console.log('js added.');
+
+    $(function () {
+        $(".feature-tile-description:not(:first-of-type)").css("display", "none");
+        $(".feature-tile-top:first-of-type").addClass("open");
+        $(".feature-tile-top").click(function () {
+          $(".open").not(this).removeClass("open").next().slideUp(300);
+          $(this).toggleClass("open").next().slideToggle(300);
+        });
+    });
 });
