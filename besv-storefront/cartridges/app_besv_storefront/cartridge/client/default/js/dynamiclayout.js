134a57f8a4d030ab8087e4f9e36a32d5b1322bf0
Shahzad Hafeez <shahzadhafeez63@gmail.com>
2022-06-18 08:41:30 +0200

    resolved pdp issues

--- besv-storefront/cartridges/app_besv_storefront/cartridge/client/default/js/dynamiclayout.js
+++ besv-storefront/cartridges/app_besv_storefront/cartridge/client/default/js/dynamiclayout.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         $(".feature-tile-description:not(:first-of-type)").css("display", "none");
         $(".feature-tile-top:first-of-type").addClass("open");
         $(".feature-tile-top").click(function () {
-          $(".open").not(this).removeClass("open").next().slideUp(300);
+          // $(".open").not(this).removeClass("open").next().slideUp(300);
           $(this).toggleClass("open").next().slideToggle(300);
         });
     });
