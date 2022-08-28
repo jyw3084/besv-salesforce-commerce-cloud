3ce51cbae6bf80b40ac01c94d92a363ed556f3eb
Shahzad Hafeez <shahzadhafeez63@gmail.com>
2022-08-28 04:57:21 +0200

    resolved BE issues

--- link_stripe/cartridges/app_stripe_sfra/cartridge/client/default/js/checkout/checkout.js
+++ link_stripe/cartridges/app_stripe_sfra/cartridge/client/default/js/checkout/checkout.js
@@ -4,12 +4,19 @@
 
 var base = require('base/checkout/checkout');
 
+var customerHelpers = require('base/checkout/customer');
 var billingHelpers = require('./billing');
 var shippingHelpers = require('base/checkout/shipping');
 var summaryHelpers = require('base/checkout/summary');
 
 base.updateCheckoutView = function () {
     $('body').on('checkout:updateCheckoutView', function (e, data) {
+
+        if (data.csrfToken) {
+            $("input[name*='csrf_token']").val(data.csrfToken);
+        }
+        customerHelpers.methods.updateCustomerInformation(data.customer, data.order);
+
         shippingHelpers.methods.updateMultiShipInformation(data.order);
         summaryHelpers.updateTotals(data.order.totals);
 
@@ -59,6 +66,19 @@ base.enableButton = function () {
 base.editPaymentButton = function () {
     $('body').on('click', '.edit-payment-btn', function (e) {
         location.reload(true);
+    });
+}
+
+base.switchHeadingStepOne = function () {
+    $('body').on('click', '.switch-guest-register', function (e) {
+        var step = '<span>1/3</span>';
+        if ($(this).hasClass('js-cancel-login')) { //guest
+            var sectionHeading = step + ' Your email';
+            $('.heading-email-my-account').html(sectionHeading);
+        } else if ($(this).hasClass('js-login-customer')) { //register
+            var sectionHeading = step + ' My account';
+            $('.heading-email-my-account').html(sectionHeading);
+        }
     });
 }
 
